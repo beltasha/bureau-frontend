c54ab63abcdecc764978137e4338991dcd986124
Natalia Beliaeva <nataly9682@gmail.com>
2019-09-27 21:36:46 +0300

    add menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
   <div id="app">
-    <p>
-    <router-link to="/feed">Лента</router-link>
-    <router-link to="/settings">Настройки</router-link>
-  </p>
+    <div class="nav-menu-container">
+      <div style="display: flex">
+        <i v-if="isCollapse" class="icon el-icon-d-arrow-right" @click="isCollapse = !isCollapse"></i>
+        <i v-if="!isCollapse" class="icon el-icon-d-arrow-left" @click="isCollapse = !isCollapse"></i>
+      </div>
 
-  <router-view></router-view>
+      <el-menu default-active="1" class="el-menu-vertical-demo" :collapse="isCollapse">
+        <router-link to="/feed">
+          <el-menu-item index="1">
+            <i class="el-icon-news"></i>
+            <span slot="title">Лента</span>
+          </el-menu-item>
+        </router-link>
+        <router-link to="/settings">
+          <el-menu-item index="2">
+            <i class="el-icon-setting"></i>
+            <span slot="title">Настройки</span>
+          </el-menu-item>
+        </router-link>
+      </el-menu>
+    </div>
+      
+    <div class="content-container">
+      <router-view></router-view>
+    </div> 
   </div>
 </template>
 
@@ -13,6 +32,11 @@
 
 export default {
   name: 'app',
+  data() {
+    return {
+      isCollapse: true
+    };
+  },
 }
 </script>
 
@@ -21,5 +45,30 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: row;
+  height: calc(100vh - 20px);
+}
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 150px;
+}
+
+.nav-menu-container {
+  display: flex;
+}
+
+.content-container {
+  display: flex;
+  flex: 1;
+  margin: 0 16px;
+}
+
+a {
+  text-decoration: none;
+}
+
+.icon {
+  cursor: pointer;
 }
 </style>
--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="settings">
-    Settings
-  </div>
+  <el-tabs v-model="activeName" class="tabs">
+    <el-tab-pane label="Подписки" name="followings">
+      Подписки
+    </el-tab-pane>
+    <el-tab-pane label="Уведомления" name="notifications">
+      Уведомления
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script>
 export default {
   name: 'Settings',
+  data() {
+      return {
+        activeName: 'followings'
+      };
+    },
+    methods: {
+    }
 }
 </script>
 
 <style scoped>
+.tabs {
+  width: 100%;
+}
 </style>
